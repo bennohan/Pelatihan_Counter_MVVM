5c3aff15e9a159c3ce757ea14e5238a5d5ee56db
bennohan <bebenbarnet@gmail.com>
2022-10-20 23:01:33 +0700

    Tanpa Hilt

--- app/src/main/java/com/example/counterone/MainActivity.kt
+++ app/src/main/java/com/example/counterone/MainActivity.kt
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.example.counterone.data.SaveCounter
 import com.example.counterone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels{
+        MainViewModel.Factory(SaveCounter(this))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.viewModel = viewModel
+
+        viewModel.sendValue()
+
     }
 
 }
